052e0a3e6e6619f61eb623eddc88fb59f8917d4f
Randy Yin <randyyin28@gmail.com>
2019-02-26 15:54:20 -0800

    Create scicafe-drop.sql

---
+++ src/main/scripts/scicafe-drop.sql
@@ -0,0 +1,18 @@
+drop table affiliations;
+drop table authorities;
+drop table event_attendance;
+drop table event_tags;
+drop table events;
+drop table hibernate_sequence;
+drop table organizations;
+drop table positions;
+drop table program_affiliations;
+drop table qualified_events;
+drop table reward_criteria;
+drop table rewards;
+drop table roles;
+drop table tags;
+drop table titles;
+drop table users;
+
+
