242c0bf07ad519e560b6422f28e02439a0af7917
Randy Yin <randyyin28@gmail.com>
2019-02-26 15:41:48 -0800

    Create Position.java

---
+++ src/main/java/springrest/model/Position.java
@@ -0,0 +1,48 @@
+package springrest.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//positions = Student, Faculty, or Staff
+@Entity
+@Table(name = "positions")
+public class Position implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Position()
+    {
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId( Long id )
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+}
