ed88e36f4193fd1b61dd434909afb8774abd0a7d
Randy Yin <randyyin28@gmail.com>
2019-02-26 15:43:45 -0800

    Create Title.java

---
+++ src/main/java/springrest/model/Title.java
@@ -0,0 +1,47 @@
+package springrest.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "titles")
+public class Title implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Title()
+    {
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId( Long id )
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+}
